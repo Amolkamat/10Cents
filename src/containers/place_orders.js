16bb3bb056ef63d1b20ce214baa82d7f5c643efe
Ilamaran Gurunathan <ilamaran.g@gmail.com>
2017-09-07 14:22:45 -0400

    Formatted menu item names display

--- src/containers/place_orders.js
+++ src/containers/place_orders.js
@@ -43,7 +43,7 @@ class PlaceOrders extends Component {
 
                   <div className="col-md-6 menu-cover-landing">
 
-                    <Panel header={menuItem.item} >
+                    <Panel header={menuItem.item + "   $" + menuItem.price} >
                       <div className = "col-md-6">
                         <select onChange={this.handleChange} >
                           <option>1</option>
