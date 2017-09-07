4ebd82160f7b83e50a6b7ec7450a471e38ec7c97
Ilamaran Gurunathan <ilamaran.g@gmail.com>
2017-09-07 04:46:41 -0400

    Grouped services seperately for code modularization

---
+++ routes/services.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+var User = require("../models/User.js");
+var CustomerOrder = require("../models/CustomerOrder");
+
+router.post('/createOrder/:id' , function(req,res) {
+  console.log(req.params.id);
+  console.log('Customer Order');
+  console.log(req.body);
+  var order = {
+    placeId: req.params.id,
+    orderList: req.body
+
+
+  }
+customerOrder = new CustomerOrder(order)
+
+  customerOrder.save(function(err, doc) {
+    // Log any errors
+    if (err) {
+      console.log(err);
+    }
+    // Or log the doc
+    else {
+      console.log(doc)
+        res.json(doc);
+    }
+  });
+
+})
+
+
+
+router.get("/api/sendMessage", function(req, res) {
+  var client = require('twilio')(
+    'ACa46cd1c81f2ea7451ad4c51cb0ba19ae',
+    'cdd0dc1bc64ca2543671709da54f6bf8'
+  );
+
+  client.messages.create({
+    from: "+17028307596",
+    to: "+17022414768",
+    body: "This is your New Order!",
+}, function(err, message) {
+  if(err){
+    console.log(err);
+    res.send({error:'error'});
+  } else {
+    console.log(message.sid);
+    res.send({success:message.sid})
+  }
+});
+
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(express.static("public"));
 // MongoDB Configuration configuration (Change this URL to your own DB)
 mongoose.connect(MONGODB);
 var db = mongoose.connection;
-var CustomerOrder = require("./models/CustomerOrder");
+
 autoIncrement.initialize(mongoose.connection);
 
 
@@ -61,53 +61,7 @@ app.get("/", function(req, res) {
 
 // This is the route we will send GET requests to retrieve our most recent search data.
 // We will call this route the moment our page gets rendered
-app.get("/api/sendMessage", function(req, res) {
-  var client = require('twilio')(
-    'ACa46cd1c81f2ea7451ad4c51cb0ba19ae',
-    'cdd0dc1bc64ca2543671709da54f6bf8'
-  );
 
-  client.messages.create({
-    from: "+17028307596",
-    to: "+17022414768",
-    body: "This is your New Order!",
-}, function(err, message) {
-  if(err){
-    console.log(err);
-    res.send({error:'error'});
-  } else {
-    console.log(message.sid);
-    res.send({success:message.sid})
-  }
-});
-
-});
-
-app.post('/services/createOrder/:id' , function(req,res) {
-  console.log(req.params.id);
-  console.log('Customer Order');
-  console.log(req.body);
-  var order = {
-    placeId: req.params.id,
-    orderList: req.body
-
-
-  }
-customerOrder = new CustomerOrder(order)
-
-  customerOrder.save(function(err, doc) {
-    // Log any errors
-    if (err) {
-      console.log(err);
-    }
-    // Or log the doc
-    else {
-      console.log(doc)
-        res.json(doc);
-    }
-  });
-
-})
 
 
 app.post( '/upload/:placeId',upload.any(), function( req, res, next ) {
@@ -147,7 +101,9 @@ User.update({"placeId":req.params.placeId},{"$set":{menuList:result["Sheet1"]}})
 
 
 var business = require('./routes/business');
+var services = require('./routes/services');
 app.use('/business', business);
+app.use('/services', services);
 
 
 
